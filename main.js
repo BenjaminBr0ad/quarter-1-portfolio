e2c8d69090ebbb592ac1d46c7b636ec07ffc9695
Benjamin J. Broad <benjamin.broad@colorado.edu>
2018-05-03 16:13:42 -0600

    Preliminary updates to contact page using FormSpree

--- main.js
+++ main.js
@@ -4,6 +4,18 @@ $( document ).ready(function(){
 
   $('.parallax').parallax(); // Parallax
 
-  $('.tooltipped').tooltip({delay: 50});
+  $('.tooltipped').tooltip({delay: 50}); // Top & bottom menu tooltips
+
+// ====== [ SCROLL FIRE ] ====== //
+  var options = [
+  {selector: '#index-reveal', offset: 200, callback: function(el) {
+    Materialize.showStaggeredList($(el));
+  } }
+];
+  Materialize.scrollFire(options);
+// ===== [ END SCROLL FIRE ] ===== //
+
+
+var controller = new ScrollMagic.Controller();
 
 }) // ===== [ END DOCUMENT READY ] ===== //
