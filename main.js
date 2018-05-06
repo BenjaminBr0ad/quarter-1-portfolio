f19e64436886bd2aecff13d1e2decc73226435a3
Benjamin J. Broad <benjamin.broad@colorado.edu>
2018-05-06 15:35:04 -0600

    Added recent projects section but need to fix ScrollMagic tweens as they are interferring with one another and then optimize to look work on desktop as well as mobile

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ $( document ).ready(function(){
   $('.parallax').parallax(); // Parallax
 
   $('.tooltipped').tooltip({delay: 50}); // Top & bottom menu tooltips
+
+  $('.materialboxed').materialbox(); // Pop-out images
 //===== [ END MATERIALIZE FUNCTIONS ] =====//
 
 //===== [ SCROLL MAGIC FUNCTIONALITY ] =====//
@@ -15,9 +17,9 @@ $( document ).ready(function(){
     duration: 1000,
     offset: 300
   })
-  .setPin("#banner-1")
-  // .addIndicators()
-  .addTo(controller);
+    .setPin("#banner-1")
+    // .addIndicators()
+    .addTo(controller);
 
   var fadeInSlogan = new ScrollMagic.Scene({
     triggerElement: "#banner-1"
@@ -32,19 +34,22 @@ $( document ).ready(function(){
     // .addIndicators({name:"1 (duration:0)"})
     .addTo(controller);
 
-  var fadeOutBanner1 = new ScrollMagic.Scene({triggerElement: "#name-text", offset: 350})
+  var fadeOutBanner1 = new ScrollMagic.Scene({
+    triggerElement: "#name-text",
+    offset: 350
+  })
     .setTween("#banner-1", 1, {opacity:0})
     // .addIndicators()
     .addTo(controller);
 //==========================================//
 //=================Banner #2================//
   var bannerPin2 = new ScrollMagic.Scene({
-    duration: 1000,
-    offset: 2250
+    duration: 2000,
+    offset: 2500
   })
-  .setPin("#banner-2")
-  // .addIndicators()
-  .addTo(controller);
+    .setPin("#banner-2")
+    // .addIndicators()
+    .addTo(controller);
 
   var fadeInBanner2 = new ScrollMagic.Scene({
     triggerElement: "#banner-2"
@@ -53,40 +58,22 @@ $( document ).ready(function(){
     // .addIndicators({name:"1 (duration:0)"})
     .addTo(controller);
 
-  // var fadeInHtml = new ScrollMagic.Scene({
-  //   triggerElement: "#banner-2"
-  // })
-  //   .setTween("#html-icon", 1, {opacity: 1})
-  //   // .addIndicators({name:"1 (duration:0)"})
-  //   .addTo(controller);
-  //
-  // var fadeInJS = new ScrollMagic.Scene({
-  //   triggerElement: "#banner-2"
-  // })
-  //   .setTween("#js-icon", 1, {opacity: 1})
-  //   // .addIndicators({name:"1 (duration:0)"})
-  //   .addTo(controller)
-  //
-  // var fadeInCSS = new ScrollMagic.Scene({
-  //   triggerElement: "#banner-2"
-  // })
-  //   .setTween("#css-icon", 1, {opacity: 1})
-  //   // .addIndicators({name:"1 (duration:0)"})
-  //   .addTo(controller)
-
-  var fadeOutBanner2 = new ScrollMagic.Scene({triggerElement: "#banner-2", offset: 1200})
+  var fadeOutBanner2 = new ScrollMagic.Scene({
+    triggerElement: "#banner-2",
+    offset: 2200
+  })
     .setTween("#banner-2", 1, {opacity:0})
-    .addIndicators()
+    // .addIndicators()
     .addTo(controller);
 //==========================================//
 //=================Banner #3================//
 var bannerPin3 = new ScrollMagic.Scene({
-  duration: 800,
-  offset: 4450
+  duration: 2000,
+  offset: 5900
 })
-.setPin("#banner-3")
-// .addIndicators()
-.addTo(controller);
+  .setPin("#banner-3")
+  // .addIndicators()
+  .addTo(controller);
 
 var fadeInBanner3 = new ScrollMagic.Scene({
   triggerElement: "#banner-3"
@@ -95,9 +82,35 @@ var fadeInBanner3 = new ScrollMagic.Scene({
   // .addIndicators({name:"1 (duration:0)"})
   .addTo(controller);
 
-var fadeOutBanner3 = new ScrollMagic.Scene({triggerElement: "#banner-3", offset: 1200})
+var fadeOutBanner3 = new ScrollMagic.Scene({
+  triggerElement: "#banner-3",
+  offset: 2200
+})
   .setTween("#banner-3", 1, {opacity:0})
   // .addIndicators()
   .addTo(controller);
+//==========================================//
+//=================Banner #4================//
+var bannerPin4 = new ScrollMagic.Scene({
+  duration: 2000,
+  offset: 9500
+})
+  .setPin("#banner-4")
+  // .addIndicators()
+  .addTo(controller);
 
+var fadeInBanner4 = new ScrollMagic.Scene({
+  triggerElement: "#banner-4"
+})
+  .setTween("#banner-4", 1, {opacity: 1})
+  .addIndicators()
+  .addTo(controller);
+
+var fadeOutBanner4 = new ScrollMagic.Scene({
+  triggerElement: "#banner-4",
+  offset: 2200
+})
+  .setTween("#banner-4", 1, {opacity: 0})
+  .addIndicators()
+  .addTo(controller);
 }) // ===== [ END DOCUMENT READY ] ===== //
