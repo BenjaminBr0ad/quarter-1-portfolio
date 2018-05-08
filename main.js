adb3266146829a9b821781abf8ad3d0d0297209c
Benjamin J. Broad <benjamin.broad@colorado.edu>
2018-05-08 14:11:18 -0600

    Media query changes such that devices below 992px will no longer pin pages which was causing major issues however the full desktop site still allows for ScrollMagic pinning

--- main.js
+++ main.js
@@ -13,16 +13,17 @@ $( document ).ready(function(){
   var controller = new ScrollMagic.Controller();
 //==========================================//
 //=================Banner #1================//
-  var bannerPin1 = new ScrollMagic.Scene({
-    duration: 1000,
-    offset: 300
-  })
+  if (window.innerWidth >= 992) {
+    var bannerPin1 = new ScrollMagic.Scene({
+      duration: 1000,
+      offset: 300
+    })
     .setPin("#banner-1")
     // .addIndicators()
     .addTo(controller);
 
-  var fadeInSlogan = new ScrollMagic.Scene({
-    triggerElement: "#banner-1"
+    var fadeInSlogan = new ScrollMagic.Scene({
+      triggerElement: "#banner-1"
     })
     .on("start", function (e) {
       TweenMax.to($("#slogan"), 0.2, {opacity: e.scrollDirection === "FORWARD" ? 1 : 0});
@@ -30,113 +31,207 @@ $( document ).ready(function(){
     // .addIndicators({name:"1 (duration:0)"})
     .addTo(controller);
 
-  var fadeInLocated = new ScrollMagic.Scene({
-    triggerElement: "#banner-1"
+    var fadeInLocated = new ScrollMagic.Scene({
+      triggerElement: "#banner-1"
     })
     .setTween("#located", 1, {opacity: 1})
     // .addIndicators({name:"1 (duration:0)"})
     .addTo(controller);
 
-  var fadeOutBanner1 = new ScrollMagic.Scene({
-    triggerElement: "#name-text",
-    offset: 350
-  })
+    var fadeOutBanner1 = new ScrollMagic.Scene({
+      triggerElement: "#name-text",
+      offset: 350
+    })
     .on("start", function (e) {
       TweenMax.to($("#banner-1"), 0.2, {opacity: e.scrollDirection === "FORWARD" ? 0 : 1});
     })
     // .addIndicators()
     .addTo(controller);
+
+  } else if (window.innerWidth < 992) {
+    var fadeInSlogan = new ScrollMagic.Scene({
+      triggerElement: "#banner-1",
+      offset: 100
+    })
+    .on("start", function (e) {
+      TweenMax.to($("#slogan"), 0.2, {opacity: e.scrollDirection === "FORWARD" ? 1 : 0});
+    })
+    .addIndicators({name:"1 (duration:0)"})
+    .addTo(controller);
+
+    var fadeInLocated = new ScrollMagic.Scene({
+      triggerElement: "#banner-1"
+    })
+    .setTween("#located", 1, {opacity: 1})
+    // .addIndicators({name:"1 (duration:0)"})
+    .addTo(controller);
+
+    var fadeOutBanner1 = new ScrollMagic.Scene({
+      triggerElement: "#name-text",
+      offset: 350
+    })
+  }
 //==========================================//
 //=================Banner #2================//
-  var bannerPin2 = new ScrollMagic.Scene({
-    duration: 2000,
-    offset: 2500
-  })
+  if (window.innerWidth >= 992) {
+    var bannerPin2 = new ScrollMagic.Scene({
+      duration: 2000,
+      offset: 2500
+    })
     .setPin("#banner-2")
     // .addIndicators()
     .addTo(controller);
 
-  var fadeInBanner2 = new ScrollMagic.Scene({
-    triggerElement: "#banner-2"
-  })
+    var fadeInBanner2 = new ScrollMagic.Scene({
+      triggerElement: "#banner-2"
+    })
     .on("start", function (e) {
       TweenMax.to($("#banner-2"), 0.2, {opacity: e.scrollDirection === "FORWARD" ? 1 : 0});
     })
     // .addIndicators({name:"1 (duration:0)"})
     .addTo(controller);
 
-  var fadeOutBanner2 = new ScrollMagic.Scene({
-    triggerElement: "#banner-2",
-    offset: 2200
-  })
+    var fadeOutBanner2 = new ScrollMagic.Scene({
+      triggerElement: "#banner-2",
+      offset: 2200
+    })
     .on("start", function (e) {
       TweenMax.to($("#banner-2"), 0.2, {opacity: e.scrollDirection === "FORWARD" ? 0 : 1});
     })
     // .addIndicators()
     .addTo(controller);
+
+  } else if (window.innerWidth < 992) {
+    var fadeInBanner2 = new ScrollMagic.Scene({
+      triggerElement: "#banner-2"
+    })
+    .on("start", function (e) {
+      TweenMax.to($("#banner-2"), 0.2, {opacity: e.scrollDirection === "FORWARD" ? 1 : 0});
+    })
+    // .addIndicators({name:"1 (duration:0)"})
+    .addTo(controller);
+
+    var fadeOutBanner2 = new ScrollMagic.Scene({
+      triggerElement: "#banner-2",
+      offset: 2200
+    })
+    .on("start", function (e) {
+      TweenMax.to($("#banner-2"), 0.2, {opacity: e.scrollDirection === "FORWARD" ? 0 : 1});
+    })
+    // .addIndicators()
+    .addTo(controller);
+  }
 //==========================================//
 //=================Banner #3================//
-  var bannerPin3 = new ScrollMagic.Scene({
-    duration: 2000,
-    offset: 5800
-  })
+  if (window.innerWidth >= 992) {
+    var bannerPin3 = new ScrollMagic.Scene({
+      duration: 2000,
+      offset: 5800
+    })
     .setPin("#banner-3")
     // .addIndicators()
     .addTo(controller);
 
-  var fadeInBanner3 = new ScrollMagic.Scene({
-    triggerElement: "#banner-3"
-  })
-  .on("start", function (e) {
-    TweenMax.to($("#banner-3"), 0.2, {opacity: e.scrollDirection === "FORWARD" ? 1 : 0});
-  })
+    var fadeInBanner3 = new ScrollMagic.Scene({
+      triggerElement: "#banner-3"
+    })
+    .on("start", function (e) {
+      TweenMax.to($("#banner-3"), 0.2, {opacity: e.scrollDirection === "FORWARD" ? 1 : 0});
+    })
     // .addIndicators({name:"1 (duration:0)"})
     .addTo(controller);
 
-  var fadeOutBanner3 = new ScrollMagic.Scene({
-    triggerElement: "#banner-3",
-    offset: 2200
-  })
-  .on("start", function (e) {
-    TweenMax.to($("#banner-3"), 0.2, {opacity: e.scrollDirection === "FORWARD" ? 0 : 1});
-  })
+    var fadeOutBanner3 = new ScrollMagic.Scene({
+      triggerElement: "#banner-3",
+      offset: 2200
+    })
+    .on("start", function (e) {
+      TweenMax.to($("#banner-3"), 0.2, {opacity: e.scrollDirection === "FORWARD" ? 0 : 1});
+    })
     // .addIndicators()
     .addTo(controller);
+
+  } else if (window.innerWidth < 992) {
+    var fadeInBanner3 = new ScrollMagic.Scene({
+      triggerElement: "#banner-3"
+    })
+    .on("start", function (e) {
+      TweenMax.to($("#banner-3"), 0.2, {opacity: e.scrollDirection === "FORWARD" ? 1 : 0});
+    })
+    // .addIndicators({name:"1 (duration:0)"})
+    .addTo(controller);
+
+    var fadeOutBanner3 = new ScrollMagic.Scene({
+      triggerElement: "#banner-3",
+      offset: 2200
+    })
+    .on("start", function (e) {
+      TweenMax.to($("#banner-3"), 0.2, {opacity: e.scrollDirection === "FORWARD" ? 0 : 1});
+    })
+    // .addIndicators()
+    .addTo(controller);
+  }
 //==========================================//
 //=================Banner #4================//
-var bannerPin4 = new ScrollMagic.Scene({
-  duration: 2000,
-  offset: 9400
-})
-  .setPin("#banner-4")
-  // .addIndicators()
-  .addTo(controller);
+  if (window.innerWidth >= 992) {
+    var bannerPin4 = new ScrollMagic.Scene({
+      duration: 2000,
+      offset: 9400
+    })
+    .setPin("#banner-4")
+    // .addIndicators()
+    .addTo(controller);
 
-var fadeInBanner4 = new ScrollMagic.Scene({
-  triggerElement: "#banner-4"
-})
-  .on("start", function (e) {
-    TweenMax.to($("#banner-4"), 0.2, {opacity: e.scrollDirection === "FORWARD" ? 1 : 0});
-  })
-  // .addIndicators()
-  .addTo(controller);
+    var fadeInBanner4 = new ScrollMagic.Scene({
+      triggerElement: "#banner-4"
+    })
+    .on("start", function (e) {
+      TweenMax.to($("#banner-4"), 0.2, {opacity: e.scrollDirection === "FORWARD" ? 1 : 0});
+    })
+    // .addIndicators()
+    .addTo(controller);
 
-var fadeOutBanner4 = new ScrollMagic.Scene({
-  triggerElement: "#banner-4",
-  offset: 2200
-})
-  .on("start", function (e) {
-    TweenMax.to($("#banner-4"), 0.2, {opacity: e.scrollDirection === "FORWARD" ? 0 : 1});
-  })
-  // .addIndicators()
-  .addTo(controller);
+    var fadeOutBanner4 = new ScrollMagic.Scene({
+      triggerElement: "#banner-4",
+      offset: 2200
+    })
+    .on("start", function (e) {
+      TweenMax.to($("#banner-4"), 0.2, {opacity: e.scrollDirection === "FORWARD" ? 0 : 1});
+    })
+    // .addIndicators()
+    .addTo(controller);
+
+  } else if (window.innerWidth < 992) {
+    var fadeInBanner4 = new ScrollMagic.Scene({
+      triggerElement: "#banner-4"
+    })
+    .on("start", function (e) {
+      TweenMax.to($("#banner-4"), 0.2, {opacity: e.scrollDirection === "FORWARD" ? 1 : 0});
+    })
+    // .addIndicators()
+    .addTo(controller);
+
+    var fadeOutBanner4 = new ScrollMagic.Scene({
+      triggerElement: "#banner-4",
+      offset: 2200
+    })
+    .on("start", function (e) {
+      TweenMax.to($("#banner-4"), 0.2, {opacity: e.scrollDirection === "FORWARD" ? 0 : 1});
+    })
+    // .addIndicators()
+    .addTo(controller);
+  }
 //==========================================//
 //=================Local Storage================//
   const renderUserInfo = function () {
+    if (!localStorage.getItem('user_data')) {
+
+    } else if (localStorage.getItem('user_data')) {
     let userInfo = JSON.parse(localStorage.getItem('user_data'))
     $('#icon_prefix').val(userInfo.name)
     $('#icon_telephone').val(userInfo.phone)
     $('#icon_email').val(userInfo.email)
+    }
   }
   renderUserInfo()
 
